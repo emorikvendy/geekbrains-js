08cca820645a0fad2b6894cc685967447e8c054c
eol morikvendy <emorikvendy@gmail.com>
2020-09-05 18:22:35 +0300

    third lesson

---
+++ third_lesson/7.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 20; console.log('x'.repeat(++i)));
+// ну или альтернативный вариант через вложенные циклы, но он хуже, на мой взгляд.
+let str = '';
+for (let i = 1; i <=20; i++){
+    str = '';
+    for (let j = 0; j < i; j++){
+        str += 'x';
+    }
+    console.log(str);
+}
