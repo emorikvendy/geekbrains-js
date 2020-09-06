fe9dc822cfa6c784281fab9628c0100c4b5f7f9a
eol morikvendy <emorikvendy@gmail.com>
2020-09-06 22:50:53 +0300

    third lesson

--- third_lesson/7.js
+++ third_lesson/7.js
@@ -1,10 +1,16 @@
-for (let i = 0; i < 20; console.log('x'.repeat(++i)));
+for (let i = 0; i < 20; console.log('x'.repeat(++i))) ;
 // ну или альтернативный вариант через вложенные циклы, но он хуже, на мой взгляд.
 let str = '';
-for (let i = 1; i <=20; i++){
+for (let i = 1; i <= 20; i++) {
     str = '';
-    for (let j = 0; j < i; j++){
+    for (let j = 0; j < i; j++) {
         str += 'x';
     }
     console.log(str);
+}
+// и третий вариант
+let output = '';
+while (output.length < 20) {
+    output += 'x';
+    console.log(output);
 }
